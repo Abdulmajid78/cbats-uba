e70e7025699778154e52ea229894545c0233b8b4
Abdumajid7 <jackodilov@gmail.com>
2024-04-04 22:41:17 +0500

    text

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,67 +1,15 @@
 'use client'
 
-import { motion } from 'framer-motion'
-import AnimatedText from './animatedText'
 import { useTranslations } from 'next-intl'
 
 const Hero = () => {
 	const t = useTranslations("Index")
-	type PlaceholderText = {
-		type: string
-		text: string
-		className: string
-		color?: string
-	}
-	const placeholderText: PlaceholderText[] = [
-		{
-			type: 'heading1',
-			text: t("Welcometo"),
-			className:
-				'md:text-3xl sm:text-[44px] ,lg:text-4xl text-start text-gray-800',
-		},
-		{
-			type: 'heading1',
-			text: t('FinanceTrends'),
-			className:
-				'md:text-4xl text-center xl:px-[20px] sm:text-[44px] text-xl text-gray-800',
-		},
-		{
-			type: 'heading2',
-			text: t("ConferenceWebsite"),
-			className:
-				'w-full sm:max-w-[1200px] max-w-[400px] mx-auto sm:text-lg text-base text-center text-gray-600 !leading-6 mt-2 lg:px-[20px]',
-			color: '#4B5563',
-		},
-		{
-			type: 'heading2',
-			text: t("TheConferenceIs"),
-			className:
-				'w-full sm:max-w-[1000px] max-w-[400px] mx-auto sm:text-lg text-base text-center text-gray-600 !leading-6 lg:px-[20px]',
-			color: '#4B5563',
-		},
-	]
-
-	const pathVariants = {
-		hidden: {
-			pathLength: 0,
-		},
-		visible: {
-			pathLength: 1,
-		},
-	}
-
-	const container = {
-		visible: {
-			transition: {
-				staggerChildren: 0.015,
-			},
-		},
-	}
+	
 
 	return (
 		<section className='min-h-[120px] container-fluid relative overflow-hidden flex flex-col justify-center pt-5 pb-8'>
 			<div className='mx-auto rounded-lg lg:-mb-5 sm:mb-0 mb-5 font-vinnytsia'>
-				<motion.div
+				{/* <motion.div
 					className='App'
 					initial='hidden'
 					animate={'visible'}
@@ -72,7 +20,16 @@ const Hero = () => {
 							return <AnimatedText {...item} key={index} />
 						})}
 					</div>
-				</motion.div>
+				</motion.div> */}
+				<div>
+					<h1 className='md:text-3xl sm:text-[44px] ,lg:text-4xl text-start text-gray-800'>{t("Welcometo")}</h1>
+					<h1 className='md:text-4xl text-center xl:px-[20px] sm:text-[44px] text-xl text-gray-800'> {t('FinanceTrends')}</h1>
+					<h1 className='md:text-4xl text-center xl:px-[20px] mt-2 sm:text-[44px] text-xl text-gray-800'>{t("date")}</h1>
+				</div>
+				<div>
+					<h2 className='w-full sm:max-w-[1200px] max-w-[400px] mx-auto sm:text-lg text-base text-center text-gray-600 !leading-6 mt-2 lg:px-[20px]'>{t("ConferenceWebsite")}</h2>
+					<h2 className='w-full sm:max-w-[1000px] max-w-[400px] mx-auto sm:text-lg text-base text-center text-gray-600 !leading-6 lg:px-[20px]'>{t("TheConferenceIs")}</h2>
+				</div>
 			</div>
 		</section>
 	)
